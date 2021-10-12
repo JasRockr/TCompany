4729622d6b9415051c8591a63eafe3ffe9cfccd4
jasrockr <jasrockr@hotmail.com>
2021-10-11 23:16:39 -0500

    Actualizacion y depuracion de archivos, se incluye la carpeta database en el proyecto

--- DB/bd_conexion.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $conn = new mysqli('fdb30.awardspace.net', '3810441_tcompany', 'DWwJny8W7!Km2T', '3810441_tcompany');
-
-    if($conn->connect_error) {
-        echo $error->$conn->connect_error;
-    }
-?>
